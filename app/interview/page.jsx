1d994d87885c8097c8f447fce8280a25fff5411d
Sambhav jain <jsambhav2004@gmail.com>
2025-04-01 21:16:23 +0530

    Update

--- app/interview/page.jsx
+++ app/interview/page.jsx
@@ -16,9 +16,6 @@ const Page = () => {
                 username: session.user.username,
                 userid: session.user.id
             },
-            headers: {
-                'Content-Type': 'application/json',
-            },
         })
     }
 
