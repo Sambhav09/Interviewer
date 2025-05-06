4b02bca18632046cfc9ed94a4159ce94fbe0b65f
Sambhav09 <144343738+Sambhav09@users.noreply.github.com>
2025-05-06 23:52:30 +0530

    signin font

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -28,7 +28,7 @@ export default function SignIn() {
     };
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+        <div className="min-h-screen text-black flex items-center justify-center bg-gray-100 px-4">
             <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
                 <h1 className="text-3xl font-bold text-center mb-6 text-gray-800">Sign In</h1>
 
